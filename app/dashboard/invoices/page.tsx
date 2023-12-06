37ab1e818436b266d749d210e78ef269beb6127c
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-12-06 13:56:03 -0300

    First Steps in NextJS

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <p className="text-xl p-2">Invoices Page</p>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div className="text-xl p-2">Dashboard Page</div>
+  )
+}
