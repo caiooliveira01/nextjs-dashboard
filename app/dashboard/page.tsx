2312ff5c4f178401eca94e6a6bf41f9d8be3a96c
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-12-12 16:09:09 -0300

    Loading data fixed

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-    <div className="text-xl p-2">Dashboard Page</div>
-  )
-}
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,43 +1,33 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from "@/app/lib/data";
 
-// This component is representational only.
-// For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
-// https://airbnb.io/visx/
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue()
+  const chartHeight = 350; // Altura do Gráfico (px)
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  const chartHeight = 350;
-  // NOTE: comment in this code when you get to this point in the course
+  const { yAxisLabels, topLabel } = generateYAxis(revenue); // Destructuring da função generateYAxis()
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
-  // if (!revenue || revenue.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  } //* Retorna o parágrafo se não existir os dados ou se for igual a 0 
 
   return (
     <div className="w-full md:col-span-4">
+
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
-      {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className="mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
           <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
+            className="hidden sm:flex mb-6 flex-col justify-between text-gray-400 text-sm"
+            style={{ height: `${chartHeight}px` }} // Tamanho do gráfico
           >
             {yAxisLabels.map((label) => (
-              <p key={label}>{label}</p>
+              <p key={label}>{label}</p> // Valores no eixo Y
             ))}
           </div>
 
@@ -45,21 +35,20 @@ export default async function RevenueChart({
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
                 className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
+                style={{ height: `${(chartHeight / topLabel) * month.revenue}px` }} // Tamanho das barras 
               ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-                {month.month}
-              </p>
+              <p className="-rotate-90 text-gray-400 text-sm sm:rotate-0">{month.month}</p> {/* Meses */}
             </div>
           ))}
         </div>
+
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+          <h3 className="text-gray-500 text-sm ml-2">Last 12 Months</h3>
         </div>
-      </div> */}
+
+      </div>
+
     </div>
   );
 }
